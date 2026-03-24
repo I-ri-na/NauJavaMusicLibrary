56ffcfdabe8a1f76d7d4218949b17066750f60b4
Irina <gira.2004@email.com>
2026-03-24 16:52:53 +0400

    Fix PR comments: remove CommandProcessor, use env vars for DB, optimize Score entity

--- src/main/java/ru/Irina/NauJava/entity/Score.java
+++ src/main/java/ru/Irina/NauJava/entity/Score.java
@@ -1,7 +1,6 @@
 package ru.Irina.NauJava.entity;
 
 import jakarta.persistence.*;
-import java.util.List;
 
 @Entity
 @Table(name = "scores")
@@ -26,21 +25,21 @@ public class Score {
     @ManyToOne
     private Category category;
 
-    @OneToMany(mappedBy = "score")
-    private List<Favorite> favorites;
-
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
+
     public String getDifficulty() { return difficulty; }
     public void setDifficulty(String difficulty) { this.difficulty = difficulty; }
+
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
+
     public Composer getComposer() { return composer; }
     public void setComposer(Composer composer) { this.composer = composer; }
+
     public Category getCategory() { return category; }
     public void setCategory(Category category) { this.category = category; }
-    public List<Favorite> getFavorites() { return favorites; }
-    public void setFavorites(List<Favorite> favorites) { this.favorites = favorites; }
 }
